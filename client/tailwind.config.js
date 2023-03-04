cbb9c266c3549b93ffae242651338837dd6abf1e
DevEss <dev.shahryar@gmail.com>
2023-03-04 12:39:01 +0330

    client config done

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    colors: {
+      black: '#181818',
+      gray: '#545454',
+      white: '#F2F2F2',
+      secondary: '#5CB8E4',
+      primary: '#8758FF',
+    },
+    fontFamily: {
+      comfortaa: ['Comfortaa', 'cursive'],
+      lato: ['Lato', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}

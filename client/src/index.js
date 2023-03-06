524af9c24d2f39ed92f13c5eec24371155db9670
DevEss <dev.shahryar@gmail.com>
2023-03-06 15:17:13 +0330

    react router added to project

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
 import React, { StrictMode } from 'react'
 import ReactDom from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from './app'
+import { Login, ErrorPage } from './pages'
 import './index.css'
+
+const router = createBrowserRouter([
+    { path: '/', element: <App />, errorElement: <ErrorPage /> },
+    { path: '/login', element: <Login /> },
+])
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 )
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+import Login from './login'
+import ErrorPage from './error-page'
+
+export { Login, ErrorPage }

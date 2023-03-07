59d5f495bd07508cba63de256aa14882fbbb7a1d
DevEss <dev.shahryar@gmail.com>
2023-03-07 11:37:00 +0330

    message done

--- client/src/components/message.jsx
+++ client/src/components/message.jsx
@@ -1,3 +1,16 @@
-export default function Message({ className }) {
-    return <div className={className}></div>
+export default function Message({
+    children,
+    bg = 'bg-success',
+    textAlign = 'text-center',
+}) {
+    const textColor = bg === 'bg-black' ? 'text-white' : 'text-black'
+    return (
+        <div className="my-5 h-auto w-full px-2 transition-all duration-500">
+            <div
+                className={`${bg} ${textColor} ${textAlign} rounded-md px-2 py-8`}
+            >
+                {children}
+            </div>
+        </div>
+    )
 }
